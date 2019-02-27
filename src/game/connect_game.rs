5139e267eed06d9df615ab6b21a7b9566cf5a80b
FaydSpeare <fspea123@gmail.com>
2019-02-27 19:47:00 +1000

    improved Game

--- src/game/connect_game.rs
+++ src/game/connect_game.rs
@@ -6,7 +6,10 @@ const MASK: u64 = 0x1FC_0000_0000;
 pub struct Game {
     pub light: u64,
     pub dark: u64,
-    pub turn: bool
+    pub turn: bool,
+
+    history: Vec<i32>,
+    moves: Vec<i32>
 }
 
 impl Game {
@@ -15,14 +18,44 @@ impl Game {
         Game {
             light: 0b0,
             dark: 0b0,
-            turn: true
+            turn: true,
+            history: Vec::new(),
+            moves: vec![0, 1, 2, 3, 4, 5, 6]
         }
+    }
+
+    pub fn undo_move(&mut self){
+
+        match self.history.pop(){
+            Some(last_move) => {
+                if !is_set((self.light|self.dark), last_move){
+                    println!("DEBUG: cannot undo move - last_move was not made");
+                }
+                self.turn = !self.turn;
+                if self.turn {
+                    self.light ^= 1 << last_move;
+                } else {
+                    self.dark ^= 1 << last_move;
+                }
+                self.moves.retain(|&e| e != last_move);
+                if last_move > 6 {
+                    self.moves.push(last_move - 7);
+                }
+            },
+            None => println!("DEBUG: cannot undo move - board is empty")
+        }
+
+
+
     }
 
     pub fn make_move(&mut self, pos: i32) {
 
+        if !self.moves.contains(&pos) {
+            println!("DEBUG: cannot make move - move not in moves");
+        }
         if is_set(self.light | self.dark, pos) {
-            panic!("DEBUG: Spot Already Taken");
+            println!("DEBUG: cannot make move - spot already taken");
         }
         if self.turn {
             self.light |= 1 << pos;
@@ -30,6 +63,14 @@ impl Game {
             self.dark |= 1 << pos;
         }
         self.turn = !self.turn;
+
+        self.moves.retain(|&e| e != pos);
+        if pos < 35 {
+            self.moves.push(pos + 7);
+        }
+
+
+        self.history.push(pos);
     }
 
     pub fn get_moves(&self) -> Vec<i32> {
@@ -57,14 +98,13 @@ impl Game {
 
         let mut moves: Vec<i32> = self.get_moves();
 
-        while ((self.light|self.dark) & MASK).count_ones() != 7 {
+        while ( (self.light|self.dark) & MASK).count_ones() != 7 {
 
 
             let r_i = rand::thread_rng().gen_range(0, moves.len());
 
             let r_move = moves[r_i];
-            moves[r_i] = moves[moves.len()-1];
-            moves.pop();
+            moves.swap_remove(r_i);
 
             if r_move + 7 < 42 {
                 moves.push(r_move+7);
--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,31 @@ mod game {
 use game::connect_game::*;
 use game::bits::bit::*;
 
+use std::time::Instant;
+
+
 fn main() {
-    for i in 0..100000 {
-        let mut g = Game::build_game();
+
+    let start = Instant::now();
+
+    let mut g;
+    for _i in 0..100000 {
+        g = Game::build_game();
         g.simulate_to_end();
     }
 
 
-    //print_i(g.light);
-    //print_i(g.dark);
+    g = Game::build_game();
+    g.make_move(0);
+
+    print_i(g.light);
+    g.undo_move();
+    print_i(g.light);
+
+    let elapsed = start.elapsed();
+
+    println!("Duration: {}", elapsed.as_secs() * 1000 +
+        elapsed.subsec_nanos() as u64 / 1_000_000);
 
 
 }
